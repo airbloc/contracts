685fdeb0e94db659d4fcc949e5ed0d9ee49cfb04
hongbyeongsu <frostornge@gmail.com>
2019-06-14 17:37:23 +0900

    env : Add coverage tools

---
+++ .solcover.js
@@ -0,0 +1,5 @@
+module.exports = {
+    norpc: true,
+    testCommand: '../node_modules/.bin/truffle test --network coverage',
+    compileCommand: '../node_modules/.bin/truffle compile --network coverage',
+}
--- truffle-config.js
+++ truffle-config.js
@@ -41,6 +41,13 @@ const config = {
             port: 9545,
             network_id: '*',
         },
+        coverage: {
+            host: '127.0.0.1',
+            network_id: '*',
+            port: 8555,
+            gas: 0xfffffffffff,
+            gasPrice: 0x01,
+        },
         klaytn: {
             host: '127.0.0.1',
             port: 8551,
