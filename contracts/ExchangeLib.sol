e0ff6539a1bb8fdcc05cf774f8ce896d6b8f53d0
hongbyeongsu <frostornge@gmail.com>
2019-06-18 17:24:34 +0900

    contract: ExchangeLib.sol * role sepatation with Exchange.sol * enhenced getter & setter * add result bytes in method `settle`

--- contracts/ExchangeLib.sol
+++ contracts/ExchangeLib.sol
@@ -1,11 +1,19 @@
 pragma solidity ^0.5.0;
+pragma experimental ABIEncoderV2;
 
 import "openzeppelin-solidity/contracts/utils/Address.sol";
+
 
 library ExchangeLib {
     using Address for address;
 
-    enum OfferStatus {NEUTRAL, PENDING, SETTLED, REJECTED}
+    /**
+     * Flow of exchange order
+     * NONE -> NEUTRAL -> PENDING |-> SETTLED
+     *    prepare     order       |-> CANCELED
+     *                            |-> REJECTED
+     */
+    enum OfferStatus {NEUTRAL, PENDING, CANCELED, SETTLED, REJECTED}
 
     struct Escrow {
         address addr;
@@ -16,7 +24,7 @@ library ExchangeLib {
     function exec(
         Escrow storage _escrow,
         bytes32 _offerId
-    ) internal returns (bool) {
+    ) internal returns (bool, bytes memory) {
         bytes memory data = abi.encode(_offerId);
         if (_escrow.args.length > 0) {
             data = abi.encodePacked(
@@ -29,12 +37,13 @@ library ExchangeLib {
     }
 
     struct Offer {
-        address     from;
-        address      to;
-        bytes20[]   dataIds;
-        Escrow      escrow;
+        string from;
+        string to;
+        bytes20[] dataIds;
+        uint256 at;
+        uint256 until;
+        Escrow escrow;
         OfferStatus status;
-        bool        reverted;
     }
 
     struct Orderbook {
@@ -42,15 +51,18 @@ library ExchangeLib {
     }
 
     function prepare(
-        Orderbook storage _orderbook,
+        Orderbook storage self,
         Offer memory _offer
     ) internal returns (bytes8) {
+        require(_offer.at == 0, "offer.at should be zero in neutral state");
+        require(_offer.until == 0, "offer.until should be zero in neutral state");
         require(_offer.status == OfferStatus.NEUTRAL, "neutral state only");
         require(_offer.escrow.addr.isContract(), "not contract address");
+
         bytes8 offerId = bytes8(
             keccak256(
                 abi.encodePacked(
-                    block.number,
+                    _offer.at,
                     msg.sender,
                     _offer.from,
                     _offer.to,
@@ -58,48 +70,103 @@ library ExchangeLib {
                 )
             )
         );
+
         _offer.status = OfferStatus.NEUTRAL;
-        _orderbook.orders[offerId] = _offer;
+        self.orders[offerId] = _offer;
+
         return offerId;
     }
 
+    function addDataIds(
+        Orderbook storage self,
+        bytes8 _offerId,
+        bytes20[] memory _dataIds
+    ) internal {
+        Offer storage offer = _get(self, _offerId);
+
+        require(offer.status == OfferStatus.NEUTRAL, "neutral state only");
+        require(offer.dataIds.length + _dataIds.length <= 255, "dataIds length exceeded (max 255)");
+
+        for (uint8 i = 0; i < _dataIds.length; i++) {
+            offer.dataIds.push(_dataIds[i]);
+        }
+    }
+
     function order(
-        Orderbook storage _orderbook,
+        Orderbook storage self,
+        bytes8 _offerId,
+        uint256 timeout
+    ) internal {
+        Offer storage offer = _get(self, _offerId);
+
+        require(offer.status == OfferStatus.NEUTRAL, "neutral state only");
+
+        offer.at = block.number;
+        offer.until = block.number + timeout;
+        offer.status = OfferStatus.PENDING;
+    }
+
+    function cancel(
+        Orderbook storage self,
         bytes8 _offerId
     ) internal {
-        Offer storage offer = _orderbook.orders[_offerId];
-        require(msg.sender == offer.from, "only offeror can order offer");
-        require(offer.status == OfferStatus.NEUTRAL, "neutral state only");
-        offer.status = OfferStatus.PENDING;
+        Offer storage offer = _get(self, _offerId);
+
+        require(offer.status == OfferStatus.PENDING, "pending state only");
+
+        offer.status = OfferStatus.CANCELED;
     }
 
     // settle and open
     function settle(
-        Orderbook storage _orderbook,
+        Orderbook storage self,
         bytes8 _offerId
-    ) internal returns (bool) {
-        Offer storage offer = _orderbook.orders[_offerId];
+    ) internal returns (bytes memory) {
+        Offer storage offer = _get(self, _offerId);
         Escrow storage escrow = offer.escrow;
+
+        require(block.number <= offer.until, "outdated order");
         require(offer.status == OfferStatus.PENDING, "pending state only");
-        require(msg.sender == offer.to, "only offeree can settle offer");
+
         offer.status = OfferStatus.SETTLED;
-        return exec(escrow, _offerId);
+
+        (bool success, bytes memory result) = exec(escrow, _offerId);
+
+        require(success, "failed to call escrow contract");
+
+        return result;
     }
 
     function reject(
-        Orderbook storage _orderbook,
+        Orderbook storage self,
         bytes8 _offerId
     ) internal {
-        Offer storage offer = _orderbook.orders[_offerId];
+        Offer storage offer = _get(self, _offerId);
+
         require(offer.status == OfferStatus.PENDING, "pending state only");
-        require(msg.sender == offer.to, "only offeree can reject offer");
+
         offer.status = OfferStatus.REJECTED;
     }
 
-    function getOffer(
-        Orderbook storage _orderbook,
+    function get(
+        Orderbook storage self,
+        bytes8 _offerId
+    ) internal view returns (Offer memory) {
+        require(exists(self, _offerId), "offer does not exist");
+        return _get(self, _offerId);
+    }
+
+    function _get(
+        Orderbook storage self,
         bytes8 _offerId
     ) internal view returns (Offer storage) {
-        return _orderbook.orders[_offerId];
+        return self.orders[_offerId];
+    }
+
+    function exists(
+        Orderbook storage self,
+        bytes8 _offerId
+    ) internal view returns (bool) {
+        return _get(self, _offerId).escrow.addr != address(0x0);
     }
 }
