abce5d293a89bed0fdfa42beaeaa86359a819a58
therne <retail3210@gmail.com>
2019-07-05 20:31:46 +0900

    Temporarily disable deployer script

--- migrations/2_deploy.js
+++ migrations/2_deploy.js
@@ -1,9 +1,9 @@
 const fs = require('fs');
 
 // const Accounts = artifacts.require("Accounts");
-const AppRegistry = artifacts.require("AppRegistry");
-const Consents = artifacts.require("Consents");
-const ConsentsLib = artifacts.require("ConsentsLib");
+const AppRegistry = artifacts.require('AppRegistry');
+const Consents = artifacts.require('Consents');
+const ConsentsLib = artifacts.require('ConsentsLib');
 // const SchemaRegistry = artifacts.require("SchemaRegistry");
 // const CollectionRegistry = artifacts.require("CollectionRegistry");
 // const Exchange = artifacts.require("Exchange");
@@ -16,52 +16,49 @@ const ConsentsLib = artifacts.require("ConsentsLib");
 
 const DEPLOYMENT_OUTPUT_PATH = 'deployment.local.json';
 
-
 module.exports = function (deployer, network) {
-    deployer.then(async () => {
-        // contracts without any dependencies will go here:
-        const baseContracts = [
-            AppRegistry,
-        ];
-        for (const contract of baseContracts) {
-            await deployer.deploy(contract);
-        }
+  deployer.then(async () => {
+    // contracts without any dependencies will go here:
+    const baseContracts = [AppRegistry];
+    for (const contract of baseContracts) {
+      await deployer.deploy(contract);
+    }
 
-        // await deployer.deploy(ConsentsLib);
-        // await deployer.link(ConsentsLib, Consents);
-        // await deployer.deploy(Consents);
+    // await deployer.deploy(ConsentsLib);
+    // await deployer.link(ConsentsLib, Consents);
+    // await deployer.deploy(Consents);
 
-        // await deployer.deploy([
-        //     Accounts,
-        //     AppRegistry,
-        //     SchemaRegistry,
-        //     Exchange,
-        //     SparseMerkleTree,
-        // ]);
-        // await deployer.deploy(CollectionRegistry, Accounts.address, AppRegistry.address, SchemaRegistry.address);
-        // await deployer.deploy(DataRegistry, Accounts.address, CollectionRegistry.address, SparseMerkleTree.address);
-        // await deployer.deploy(SimpleContract, Exchange.address);
-        // // test
-        // await deployer.deploy(ERC20Mintable);
-        //
-        // const deployments = {
-        //     Accounts: Accounts.address,
-        //     AppRegistry: AppRegistry.address,
-        //     SchemaRegistry: SchemaRegistry.address,
-        //     CollectionRegistry: CollectionRegistry.address,
-        //     Exchange: Exchange.address,
-        //     SimpleContract: SimpleContract.address,
-        //     SparseMerkleTree: SparseMerkleTree.address,
-        //     DataRegistry: DataRegistry.address,
-        // };
-        //
-        // if (network === 'test' || network === 'local') {
-        //     deployments.ERC20Mintable = ERC20Mintable.address;
-        // }
-        //
-        // console.log('Writing deployments to deployment.local.json');
-        // fs.writeFileSync(DEPLOYMENT_OUTPUT_PATH, JSON.stringify(deployments, null, '  '));
-        //
-        // console.log('You should run "make generate-bind" if any ABIs have been changed.');
-    });
+    // await deployer.deploy([
+    //     Accounts,
+    //     AppRegistry,
+    //     SchemaRegistry,
+    //     Exchange,
+    //     SparseMerkleTree,
+    // ]);
+    // await deployer.deploy(CollectionRegistry, Accounts.address, AppRegistry.address, SchemaRegistry.address);
+    // await deployer.deploy(DataRegistry, Accounts.address, CollectionRegistry.address, SparseMerkleTree.address);
+    // await deployer.deploy(SimpleContract, Exchange.address);
+    // // test
+    // await deployer.deploy(ERC20Mintable);
+    //
+    // const deployments = {
+    //     Accounts: Accounts.address,
+    //     AppRegistry: AppRegistry.address,
+    //     SchemaRegistry: SchemaRegistry.address,
+    //     CollectionRegistry: CollectionRegistry.address,
+    //     Exchange: Exchange.address,
+    //     SimpleContract: SimpleContract.address,
+    //     SparseMerkleTree: SparseMerkleTree.address,
+    //     DataRegistry: DataRegistry.address,
+    // };
+    //
+    // if (network === 'test' || network === 'local') {
+    //     deployments.ERC20Mintable = ERC20Mintable.address;
+    // }
+    //
+    // console.log('Writing deployments to deployment.local.json');
+    // fs.writeFileSync(DEPLOYMENT_OUTPUT_PATH, JSON.stringify(deployments, null, '  '));
+    //
+    // console.log('You should run "make generate-bind" if any ABIs have been changed.');
+  });
 };
