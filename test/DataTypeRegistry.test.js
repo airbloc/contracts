febc91ec4cb72f0a9c60334b5090b34f11229ac0
hongbyeongsu <frostornge@gmail.com>
2019-07-11 12:20:31 +0900

    test: DataTypeRegistry - Migrate to openzeppelin-test-helper

--- test/DataTypeRegistry.test.js
+++ test/DataTypeRegistry.test.js
@@ -1,4 +1,4 @@
-const truffleAssert = require('truffle-assertions');
+const { expectEvent, expectRevert } = require('openzeppelin-test-helpers');
 const { expect } = require('./test-utils');
 
 const DataTypeRegistry = artifacts.require('DataTypeRegistry');
@@ -11,6 +11,7 @@ const DATA_SCHEMA = JSON.stringify({
 const SCHEMA_HASH = web3.utils.sha3(DATA_SCHEMA);
 
 contract('DataTypeRegistry', async (ethAccounts) => {
+  const name = 'test-data';
   const [me, stranger] = ethAccounts;
   let dataTypes;
 
@@ -20,59 +21,62 @@ contract('DataTypeRegistry', async (ethAccounts) => {
 
   describe('#register()', () => {
     it('should done correctly', async () => {
-      const result = await dataTypes.register('test-data', SCHEMA_HASH);
-      truffleAssert.eventEmitted(result, 'Registration', event => event.name === 'test-data');
+      const { logs } = await dataTypes.register(name, SCHEMA_HASH);
+      expectEvent.inLogs(logs, 'Registration', { name });
     });
 
     it('should fail if the name duplicates', async () => {
-      await truffleAssert.passes(dataTypes.register('test-data', SCHEMA_HASH));
-      await truffleAssert.fails(
-        dataTypes.register('test-data', SCHEMA_HASH),
-        truffleAssert.ErrorType.REVERT,
-        'already exists',
+      await dataTypes.register(name, SCHEMA_HASH);
+      await expectRevert(
+        dataTypes.register(name, SCHEMA_HASH),
+        'DataTypeRegistry: data type name already exists',
       );
     });
   });
 
   describe('#get()', () => {
     it('should retrieve registered app data', async () => {
-      await dataTypes.register('test-data', SCHEMA_HASH);
+      await dataTypes.register(name, SCHEMA_HASH);
 
-      const dataType = await dataTypes.get('test-data');
-      expect(dataType.name).to.equal('test-data');
+      const dataType = await dataTypes.get(name);
+      expect(dataType.name).to.equal(name);
       expect(dataType.schemaHash).to.equal(SCHEMA_HASH);
       expect(dataType.owner).to.equal(me);
     });
 
     it('should fail on unregistered app name', async () => {
-      await truffleAssert.fails(dataTypes.get('unregistered-app'), truffleAssert.ErrorType.REVERT);
+      await expectRevert(
+        dataTypes.get('unregistered-app'),
+        'DataTypeRegistry: data type does not exist',
+      );
     });
   });
 
   describe('#exists()', () => {
     it('should able to check existence', async () => {
-      await dataTypes.register('test-data', SCHEMA_HASH);
+      await dataTypes.register(name, SCHEMA_HASH);
 
-      await expect(dataTypes.exists('test-data')).to.eventually.be.true;
+      await expect(dataTypes.exists(name)).to.eventually.be.true;
       await expect(dataTypes.exists('wrong-test-data')).to.eventually.be.false;
     });
   });
 
   describe('#unregister()', () => {
     it('should unregister correctly', async () => {
-      await dataTypes.register('test-data', SCHEMA_HASH);
+      await dataTypes.register(name, SCHEMA_HASH);
 
-      const result = await dataTypes.unregister('test-data');
-      truffleAssert.eventEmitted(result, 'Unregistration', event => event.name === 'test-data');
+      const { logs } = await dataTypes.unregister(name);
+      expectEvent.inLogs(logs, 'Unregistration', { name });
+
+      await expectRevert(dataTypes.get(name), 'DataTypeRegistry: data type does not exist');
     });
 
     it('should fail to unregister if it is not by the owner', async () => {
-      await dataTypes.register('test-data', SCHEMA_HASH);
+      await dataTypes.register(name, SCHEMA_HASH);
 
-      await truffleAssert.fails(
-        dataTypes.unregister('test-data', { from: stranger }),
-        truffleAssert.ErrorType.REVERT,
-        'unauthorized',
+      await expectRevert(
+        dataTypes.unregister(name, { from: stranger }),
+        'DataTypeRegistry: unauthorized',
       );
     });
   });
