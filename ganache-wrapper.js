355acad99b35c9e182c27df0074701df3e71f389
hongbyeongsu <frostornge@gmail.com>
2019-07-17 16:24:51 +0900

    env: Linter

--- ganache-wrapper.js
+++ ganache-wrapper.js
@@ -1,6 +1,14 @@
 const fs = require('fs');
 const http = require('http');
-const spawn = require('child_process').spawn;
+const { spawn } = require('child_process');
+
+function startGanache(customArguments) {
+  if (process.env.DOCKER) {
+    return spawn('node', ['/app/ganache-core.docker.cli.js', '-i', '1337', ...customArguments]);
+  }
+  // assume that ganache-cli is installed
+  return spawn('ganache-cli', ['-i', '1337', ...customArguments]);
+}
 
 const customArguments = process.argv.slice(2);
 const ganache = startGanache(customArguments);
@@ -14,12 +22,4 @@ process.on('exit', () => ganache.kill());
 
 // add deployment server
 http.createServer((req, res) => fs.createReadStream('deployment.local.json').pipe(res))
-    .listen(8500, () => console.log('Deployment info server started at http://localhost:8500'));
-
-function startGanache(customArguments) {
-    if (process.env['DOCKER']) {
-        return spawn('node', ['/app/ganache-core.docker.cli.js', '-i', '1337', ...customArguments]);
-    }
-    // assume that ganache-cli is installed
-    return spawn('ganache-cli', ['-i', '1337', ...customArguments]);
-}
+  .listen(8500, () => console.log('Deployment info server started at http://localhost:8500'));
