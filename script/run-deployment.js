be784fc01ab955506c5bc193374b42f5cf57fd25
orngefrost <frostornge@gmail.com>
2019-08-23 03:47:58 +0900

    env: Change script for support all environment (local, test, main)

--- script/run-deployment.js
+++ script/run-deployment.js
@@ -1,6 +1,19 @@
 const fs = require('fs');
 const http = require('http');
 
-// add deployment server
-http.createServer((req, res) => fs.createReadStream('deployment.local.json').pipe(res))
-  .listen(8500, () => console.log('Deployment info server started at http://localhost:8500'));
+const env = process.argv[2]; // 'test', 'local', None
+const path = `deployment${env ? `.${env}` : ''}.json`;
+
+const host = 'localhost';
+const port = 8500;
+
+if (fs.existsSync(path)) {
+  // add deployment server
+  http.createServer((req, res) => fs.createReadStream(path).pipe(res))
+    .listen(port, () => {
+      console.log(`Deployment info server started at http://${host}:${port}.`);
+      console.log(`Deployment path is ./${path}`);
+    });
+} else {
+  console.error(`file ${path} does not exist`);
+}
