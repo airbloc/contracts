2596ff63f53520fc48eed4ebbb195a15321fd99d
hongbyeongsu <frostornge@gmail.com>
2020-06-05 19:09:40 +0900

    feat: DataRegistry interface

---
+++ contracts/DataRegistry.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.5.0;
+pragma experimental ABIEncoderV2;
+
+
+contract DataRegistry {
+    function register() public returns (bytes8);
+    function unregister() public;
+    function get() public;
+    function getId() public returns (bytes8);
+}
+
